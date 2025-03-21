306d9813e88e0aa932e22958b3d797effa162a29
Vincent Heuer <vheuer@icloud.com>
2025-03-21 19:43:31 +0100

    added dataset class

---
+++ week3/dataset.py
@@ -0,0 +1,46 @@
+from torch.utils.data import Dataset
+import torch
+from torchvision import transforms as v2
+from PIL import Image
+from constants import TEXT_MAX_LEN, CHAR2IDX, CHARS
+
+# Path needs to be added
+
+
+class FoodDataset(Dataset):
+    def __init__(self, annotations):
+        """Dataset for loading food images and their corresponding captions."""
+        self.annotations = annotations
+        self.max_len = TEXT_MAX_LEN
+
+        # Define transformations
+        self.transforms = v2.Compose([
+            v2.ToImage(),
+            v2.ToDtype(torch.float32, scale=True),
+            v2.Resize((224, 224), antialias=True),
+            v2.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+        ])
+
+    def __len__(self):
+        return len(self.annotations)
+    
+    def __getitem__(self, idx):
+        """Returns an image tensor, the raw caption, and the tokenized caption indices."""
+        item = self.annotations.iloc[idx]
+
+        # Load and process image
+        img_path = "Path needs to be added" / item.Image_Name
+        img = Image.open(img_path).convert('RGB')
+        img = self.transforms(img)
+    
+        # Process caption
+        caption = item.Title  # Original caption
+        cap_tokens = [CHARS[0]] + list(caption) + [CHARS[1]]  # Add <SOS> and <EOS>
+
+        # Pad the caption if necessary
+        cap_tokens += [CHARS[2]] * (self.max_len - len(cap_tokens))
+
+        # Convert characters to indices
+        cap_idx = [CHAR2IDX[char] for char in cap_tokens]
+
+        return img, caption, cap_idx
--- week3/utils.py
+++ week3/utils.py
@@ -4,18 +4,18 @@ import numpy as np
 # debuggin
 # path = "C:/Users/Vincent Heuer/OneDrive - Berode GmbH/Dokumente/Master/C5_project/archive/Food Ingredients and Recipe Dataset with Image Name Mapping.csv"
 
-def get_annotations(path):
+def load_annotations(path):
     """Loads the annotations CSV and ensures image filenames have a '.jpg' extension."""
     df = pd.read_csv(path, index_col=0)
     df['Image_Name'] = df['Image_Name'].astype(str) + '.jpg'
     return df
 
-def get_train_val_test_annotations_split(path, splits=(0.8, 0.1, 0.1)):
+def get_annotation_split(path, splits=(0.8, 0.1, 0.1)):
     """Splits the annotations dataset into train, validation, and test sets based on given proportions."""
     if not np.isclose(sum(splits), 1.0):
         raise ValueError("Splits must sum to 1.")
 
-    df = get_annotations(path)
+    df = load_annotations(path)
     n_samples = len(df)
     
     train_size = int(splits[0] * n_samples)
@@ -34,4 +34,3 @@ def get_train_val_test_annotations_split(path, splits=(0.8, 0.1, 0.1)):
     }
 
 
-print(get_train_val_test_annotations_split(path))
