2b7df56442bf49d0e09d6b0fcdc6ef6a341ab503
philzett11 <philzett@icloud.com>
2025-03-23 22:48:42 +0100

    Delete week3/constants.py

--- week3/constants.py
+++
@@ -1,7 +0,0 @@
-# Constants needed throughout the files
-
-CHARS = ['<SOS>', '<EOS>', '<PAD>', ' ', '!', '"', '#', '&', "'", '(', ')', ',', '-', '.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '=', '?', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-NUM_CHAR = len(CHARS)
-IDX2CHAR = {k: v for k, v in enumerate(CHARS)}
-CHAR2IDX = {v: k for k, v in enumerate(CHARS)}
-TEXT_MAX_LEN = 201
